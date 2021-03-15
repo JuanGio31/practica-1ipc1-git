c37313e7fe7ba2a5e1efa509a879db18d4dd05cd
JuanGio31 <giovanicasia@gmail.com>
2021-03-15 02:23:47 -0600

    123

---
+++ src/Personajes.java
@@ -0,0 +1,17 @@
+public class Personajes {
+
+    protected int hp;
+    protected int armadura;
+    protected int ataque;
+    protected String nombre;
+    protected boolean esHeroe;
+    
+    public Personajes(int hp, int armadura, int ataque, String nombre, boolean esHeroe){
+        this.hp = hp;
+        this.armadura = armadura;
+        this.ataque = ataque;
+        this.nombre = nombre;
+        this.esHeroe = esHeroe;
+
+    }
+}
